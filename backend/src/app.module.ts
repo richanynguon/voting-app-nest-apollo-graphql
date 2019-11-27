6457dc0125d24b5aa4f6de53ffb61e3553dfb134
richanynguon <alina.nguon@gmail.com>
2019-11-27 10:05:15 -0500

    create user modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// #3
+import { GraphQLModule } from "@nestjs/graphql"; 
+import { UserModule } from './user/user.module';
+
 
 // #2
 @Module({
-  imports: [],
+  imports: [
+    // #4
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    // #5 $nest g module user
+    // this command will import UserModule and create user folder
+    UserModule,  
+    
+  ],
   // Importing app controllers that are the methods and mutations
   // like controllers in express
   controllers: [AppController],
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+// Like models in express connects CRUD func with db response
 @Injectable()
 export class AppService {
   getHello(): string {
