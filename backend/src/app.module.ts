1b45c1cdd7cba449c72d99779fced1f8e02ebe7a
richanynguon <alina.nguon@gmail.com>
2019-11-27 09:30:50 -0500

    add graphql dependencies and wrote notes in boiler plate

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// #2
 @Module({
   imports: [],
+  // Importing app controllers that are the methods and mutations
+  // like controllers in express
   controllers: [AppController],
+  // Importing app service - contains method and mutation helpers
+  // Think models in express
   providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+// #1 
 async function bootstrap() {
+  // Creating entry module which points to AppModule
   const app = await NestFactory.create(AppModule);
+  // Instaniates server
   await app.listen(3000);
 }
 bootstrap();
