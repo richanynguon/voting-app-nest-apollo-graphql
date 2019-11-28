8408417d384c2ef4518336430893dc77fd33e190
richanynguon <alina.nguon@gmail.com>
2019-11-28 05:54:51 -0500

    complete sessions flow on login

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,20 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as session from 'express-session'
 import { SESSION_SECRET } from './constants';
+import * as Store from 'connect-redis';
+import { redis } from './redis';
+
 
 dotenv.config()
 // #1 $ npm i --save @nestjs/graphql apollo-server-express graphql-tools graphql
 async function bootstrap() {
   // Creating entry module which points to AppModule
+  const RedisStore = Store(session);
   const app = await NestFactory.create(AppModule);
   app.use(session({
+    store: new RedisStore({
+      client: redis as any,
+    }),
     name: "votingapp",
     secret: SESSION_SECRET,
     resave: false,
@@ -17,6 +24,8 @@ async function bootstrap() {
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
+      maxAge: 1000 * 60 * 60 * 24 *365,
+
     },
 
   }))
